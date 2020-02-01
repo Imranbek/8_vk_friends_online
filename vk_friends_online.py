38b14623c3640dbe6c0e18a7f2b7a1ad6491c0c3
Imranbek <imranbek.aliev@pimpay.ru>
2020-02-01 04:13:08 +0300

    vk_friends_online finished

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,60 @@
-import vk
+import getpass
+
+import vk_requests
+
+APP_ID = 7081621
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+def main():
+    login = get_user_login()
+    password = get_user_password()
+    vk_api = connect_vk_api(login=login,
+                            password=password)
+    friends_online = get_online_friends(vk_api)
+    output_friends_to_console(friends_online)
 
 
 def get_user_login():
-    pass
+    user_login = input('Please enter your VK login:')
+    assert user_login != '', 'Login should not be empty string.'
+    return user_login
 
 
 def get_user_password():
-    pass
+    user_password = getpass.getpass("Please enter your password for check "
+                                    "and press Enter:")
+    assert user_password != '', 'Password should not be empty string.'
+    return user_password
 
 
-def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+def connect_vk_api(login,
+                   password):
+    api = vk_requests.create_api(app_id=APP_ID,
+                                 login=login,
+                                 password=password,
+                                 api_version='5.101',
+                                 scope='friends')
+    return api
+
+
+def get_online_friends(api):
+    f = api.friends.getOnline()
+    friends_online = (api.users.get(user_ids=f))
+
+    return friends_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    friends_online_number = len(friends_online)
+    print('Number of friends online : {}'.format(friends_online_number))
+    if friends_online_number == 0:
+        exit
+
+    for idx, friend in enumerate(friends_online, start=1):
+        print('{}: {} {}'.format(idx,
+                                 friend['first_name'],
+                                 friend['last_name']))
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    main()
